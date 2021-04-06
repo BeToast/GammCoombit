d973ff508781508c8b616b3cb2da15a184fddba2
SPIpercy <57994638+SPIpercy@users.noreply.github.com>
2021-04-06 19:27:17 +0100

    gamm cumbit

--- board.js
+++ board.js
@@ -1,8 +1,6 @@
 var x=4;
 var y=4;
-
 var chessBoard = document.getElementById("chessBoard");
-
 for (var i=0; i<y; i++){
     var row = chessBoard.appendChild(document.createElement("div"));
     for (var j=0; j<x; j++){

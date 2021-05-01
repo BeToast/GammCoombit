7532cd85d9c2029a3e6d3c1456582757b986f5e6
Blake <note2bp@gmail.com>
2021-05-01 15:34:35 -0700

    Trying to make board work

--- main page/board.js
+++ main page/board.js
@@ -1,7 +1,7 @@
 var chessBoard = document.getElementById("chessBoard");
-for (var i=4; i>=1; i--){
+for (var i=3; i>=0; i--){
     var row = chessBoard.appendChild(document.createElement("div"));
-    for (var j=4; j>=1; j--){
+    for (var j=3; j>=0; j--){
         square = document.createElement("span");
         square.id = generateId(i, j);
         row.appendChild(square);
@@ -11,20 +11,50 @@ for (var i=4; i>=1; i--){
 function generateId (i, j) {
     var id = "";
     switch(j) {
-        case 4:
-            return(id = ("a"+i+""));
         case 3:
-            return(id = ("b"+i+""));
+            return(id = ("a"+(i+1)+""));
         case 2:
-            return(id = ("c"+i+""));
+            return(id = ("b"+(i+1)+""));
         case 1:
-            return(id = ("d"+i+""));
+            return(id = ("c"+(i+1)+""));
+        case 0:
+            return(id = ("d"+(i+1)+""));
     }
 }
 function settings() {
-  document.getElementById("nav-link1").style.display = "active";
+    document.getElementById("nav-link1").style.display = "active";
 }
 
 function off() {
-  document.getElementById("chessBoard").style.display = "none";
+    document.getElementById("chessBoard").style.display = "none";
 }
+
+function newBoard(newBoard){
+    var board = document.getElementById("chessBoard");
+    for(let e of board.getElementByTagName("span")){
+        
+    }
+}
+
+
+var defaultBoard = new Array(4);
+for(var i = 0; i < 4; i++){
+    defaultBoard[i] = new Array(4);
+}
+defaultBoard[0][0] = "0";
+defaultBoard[0][1] = "0";
+defaultBoard[0][2] = "bR";
+defaultBoard[0][3] = "bK";
+defaultBoard[1][0] = "0";
+defaultBoard[1][1] = "0";
+defaultBoard[1][2] = "bB";
+defaultBoard[1][3] = "bR";
+defaultBoard[2][0] = "wR";
+defaultBoard[2][1] = "wB";
+defaultBoard[2][2] = "0";
+defaultBoard[2][3] = "0";
+defaultBoard[3][0] = "wK";
+defaultBoard[3][1] = "wR";
+defaultBoard[3][2] = "0";
+defaultBoard[3][3] = "0";
+

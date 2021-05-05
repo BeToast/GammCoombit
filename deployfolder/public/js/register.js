b599db1400ca7e457333cd658ef75756142935f9
GimmeDaCookie <szemetukasz@gmail.com>
2021-05-05 16:33:39 +0100

    Update register.js

--- deployfolder/public/js/register.js
+++ deployfolder/public/js/register.js
@@ -4,6 +4,7 @@ function register()
     let password = document.getElementById('passwordField').value
     firebase.auth().createUserWithEmailAndPassword(email, password)
 	.then(function(user){
+		var user = firebase.auth().currentUser;
 		user.updateProfile({
         displayName: document.getElementById("nicknameField").value
     })

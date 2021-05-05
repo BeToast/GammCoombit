4c3917b12fc16b2f0ce280e5de37870b015f9631
GimmeDaCookie <szemetukasz@gmail.com>
2021-05-05 01:04:29 +0100

    names shall work righjt fucking now blyart

--- deployfolder/public/js/chat.js
+++ deployfolder/public/js/chat.js
@@ -31,7 +31,7 @@
       msgRef.on('child_added', updateMsgs);
   }
   const updateMsgs = data => {
-    const {email: userEmail ,name: name, text} = data.val();
+    const {email,name, text} = data.val();
     var outputText = text;
     const msg = `<li class="${email == userEmail ? "msg my": "msg"}"><span class = "msg-span">
       <i class = "name">${name}: </i>${outputText}

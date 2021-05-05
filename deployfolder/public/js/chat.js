ab29312835faa0f52825d651cbfa5b31ad1f2262
GimmeDaCookie <szemetukasz@gmail.com>
2021-05-05 01:39:32 +0100

    rfghuyir
    regwerggrewgre

--- deployfolder/public/js/chat.js
+++ deployfolder/public/js/chat.js
@@ -31,7 +31,7 @@
       msgRef.on('child_added', updateMsgs);
   }
   const updateMsgs = data => {
-    const {userEmail,name, text} = data.val();
+    const {email:userEmail,name, text} = data.val();
     var outputText = text;
     const msg = `<li class="${email == userEmail ? "msg my": "msg"}"><span class = "msg-span">
       <i class = "name">${name}: </i>${outputText}
